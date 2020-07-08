795c37103d8c68ccc9182e37d010923735389c10
Monika <monint96@gmail.com>
2020-07-08 11:40:34 +0300

    Second Project

---
+++ 2 Counter/main.js
@@ -0,0 +1,33 @@
+var numberPlace = document.getElementById("number");
+var number = 0;
+
+var decrease = document.getElementById("decrease");
+decrease.addEventListener("click", function () {
+  number -= 1;
+  numberPlace.textContent = number;
+  colorChange();
+});
+
+var increase = document.getElementById("increase");
+increase.onclick = function () {
+  number += 1;
+  numberPlace.textContent = number;
+  colorChange();
+};
+
+var reset = document.getElementById("reset");
+reset.onclick = function () {
+  number = 0;
+  numberPlace.textContent = number;
+  colorChange();
+};
+
+function colorChange() {
+  if (number <= -1) {
+    numberPlace.style.color = "red";
+  } else if (number > 0) {
+    numberPlace.style.color = "green";
+  } else {
+    numberPlace.style.color = "inherit";
+  }
+}
