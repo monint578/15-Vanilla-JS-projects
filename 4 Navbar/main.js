5c5663841031546ecae4cd6e725836030ec34a00
Monika <monint96@gmail.com>
2020-08-05 15:38:07 +0300

    NavBar Initial commit

---
+++ 4 Navbar/main.js
@@ -0,0 +1,6 @@
+function showNavigation() {
+  var navLinks = document.getElementById("nav-links")
+  navLinks.classList.toggle("active")
+  var button = document.getElementById("btn")
+  button.classList.toggle("active-btn")
+}
